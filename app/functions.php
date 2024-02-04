7eb05f3bd00259478451789a76d24f0e68e3d614
Abdul Hafidz Anshari <aditans88@gmail.com>
2024-02-04 19:22:28 +0700

    update

---
+++ app/functions.php
@@ -0,0 +1,116 @@
+<?php
+
+use Hyperf\Utils\Arr;
+use Hyperf\Utils\Codec\Json;
+use Hyperf\Utils\ApplicationContext;
+use Hyperf\HttpMessage\Stream\SwooleStream;
+use Hyperf\HttpServer\Contract\ResponseInterface;
+
+
+if( ! function_exists('response')) {
+
+    function response($data, int $code = 0, array $meta = [])
+    {
+        $response = ApplicationContext::getContainer()->get(ResponseInterface::class);
+        $payload = [
+            'error' => $code
+        ];
+        if(is_string($data)) {
+            $payload['message'] = $data;
+            $data = null;
+        }
+
+        if ($data || is_array($data)) {
+            $payload['data'] = $data;
+        }
+
+        if ($meta) {
+            $payload['meta'] = $meta;
+        }
+
+        if($data instanceof \Hyperf\Resource\Json\AnonymousResourceCollection || $data instanceof \Hyperf\Resource\Json\ResourceCollection){
+            if($data->resource instanceof \Hyperf\Paginator\LengthAwarePaginator) {
+                $payload['meta'] = Arr::except($data->resource->toArray(), [
+                    'data',
+                    'first_page_url',
+                    'last_page_url',
+                    'prev_page_url',
+                    'next_page_url',
+                ]);
+            }
+        }
+
+        // $payload = mb_convert_encoding($payload, 'UTF-8', 'UTF-8');
+        $payload = Json::encode($payload);
+        
+        return $response
+                ->withStatus(200)
+                ->withHeader('content-type', 'application/json')
+                ->withBody(new SwooleStream($payload));
+    }
+}
+
+if( ! function_exists('export')) {
+    function export(array $head, array $body, string $file_name) {
+        
+        $head_keys   = array_keys($head);
+        $head_values = array_values($head);
+        $fileData    = implode(',', $head_values) . "\n";
+ 
+        if (strpos($file_name, '.') === false) {
+            $file_name .= '.csv';
+        }
+ 
+        foreach ($body as $value) {
+            $temp_arr = [];
+            $value = json_decode(json_encode($value), true);
+            foreach ($head_keys as $key) {
+                $temp_arr[] = $value[$key] ?? '';
+            }
+            $fileData .= implode(',', $temp_arr) . "\n";
+        }
+ 
+        $response = ApplicationContext::getContainer()->get(ResponseInterface::class);
+        $content_type = 'text/csv';
+
+        return $response->withHeader('content-description', 'File Transfer')
+            ->withHeader('content-type', $content_type)
+            ->withHeader('content-disposition', "attachment; filename={$file_name}")
+            ->withHeader('content-transfer-encoding', 'binary')
+            ->withHeader('pragma', 'public')
+            ->withBody(new SwooleStream($fileData));
+    }
+}
+
+if(! function_exists('fossnir_file')) {
+    function fossnir_file($mill_id) {
+        $dir = \App\Model\FossnirDir::find($mill_id);
+        if($dir) {
+            $smb = make(\App\Service\Samba::class);
+            $files = $smb->dir($dir->dir_path);
+            $count = 1;
+            foreach($files as $file) {
+                echo "filename : " .  $file->getName() . " date: ". $file->getMTime() . "\n";
+                $count++;
+            }
+            echo "Jumlah File: " . $count;
+        }
+    }
+}
+
+function tx($lm) {
+    $levels = [0, 1000, 2000, 3000, 4000, 5000, 6000, 7000];
+    $temps = [];
+    foreach($levels as $k => $v) {
+        if($lm >= $v) {
+            $rand = rand(30, 50);
+            echo $rand . "\n";
+            $temps[] = $rand;
+        }
+    }
+    echo "----- \n";
+    var_dump($temps);
+    echo "----- \n";
+    $temp_tank = round(array_sum($temps) / count($temps));
+    return $temp_tank;
+}
