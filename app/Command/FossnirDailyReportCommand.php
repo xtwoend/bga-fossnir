9839ef3a575b9b00e3981e8a695f776b797520b5
Abdul Hafidz Anshari <aditans88@gmail.com>
2024-02-28 14:22:05 +0700

    update

--- app/Command/FossnirDailyReportCommand.php
+++ app/Command/FossnirDailyReportCommand.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace App\Command;
 
 use Carbon\Carbon;
+use App\Model\Group;
 use App\Model\FossnirDir;
 use App\Model\FossnirData;
 use App\Model\GroupProduct;
 use Hyperf\DbConnection\Db;
 use App\Model\FossnirThreshold;
+use App\Model\FossnirReportDaily;
 use Psr\Container\ContainerInterface;
 use Hyperf\Command\Annotation\Command;
 use Hyperf\Command\Command as HyperfCommand;
+use Symfony\Component\Console\Input\InputArgument;
 
 #[Command]
 class FossnirDailyReportCommand extends HyperfCommand
@@ -28,58 +31,75 @@ class FossnirDailyReportCommand extends HyperfCommand
         $this->setDescription('Save daily result fossnir per mill');
     }
 
+    protected function getArguments()
+    {
+        return [
+            ['date', InputArgument::OPTIONAL, 'date']
+        ];
+    }
+
     public function handle()
     {
         // result
-        $now = Carbon::now();
-        $from = Carbon::parse($now->format('Y-m-d') . ' 05:00:00')->subDay()->format('Y-m-d H:i:s');
-        $to = Carbon::parse($now->format('Y-m-d') . ' 05:00:00')->format('Y-m-d H:i:s');
+        $date = $this->input->getArgument('date');
+        $now = $date ? Carbon::parse($date): Carbon::now();
+        $from = Carbon::parse($now->format('Y-m-d') . ' 05:00:00')->format('Y-m-d H:i:s');
+        $to = Carbon::parse($now->format('Y-m-d') . ' 05:00:00')->addDay()->format('Y-m-d H:i:s');
         $interval = 2;
 
         foreach(FossnirDir::all() as $dir){
             
             // calculate bedasarkan group fossnir mechines
-            $groups = FossnirData::select('product_name')
-                ->where('mill_id', $dir->id)
-                ->whereBetween('sample_date', [$from, $to])
-                ->groupBy('product_name')
-                ->get()->pluck('product_name')->toArray();
-
-            $prs = [];
-            foreach($groups as $g) {
-                $pr = FossnirData::table($dir->id)
-                    ->select(Db::raw("count(*) as total, avg(owm) as owm, avg(vm) as vm, avg(odm) as odm, avg(nos) as nos"))
-                    ->where('product_name', $g)
-                    ->whereBetween('sample_date', [$from, $to])
-                    ->groupBy('product_name')
-                    ->get()
-                    ->first();
+            $groups = Group::all();
+            foreach($groups as $group) {
+                $groupProducts = GroupProduct::where('mill_id', $dir->id)->where('group_id', $group->id)->get();
                 
-                if($pr) {  
-                    $prs[] = $pr->toArray();
+                $prs = [];
+                foreach($groupProducts as $g) {
+                    $pr = FossnirData::table($dir->id)
+                        ->select(Db::raw("count(*) as total, avg(owm) as owm, avg(vm) as vm, avg(odm) as odm, avg(nos) as nos"))
+                        ->where('product_name', $g->product_name)
+                        ->whereBetween('sample_date', [$from, $to])
+                        ->groupBy('product_name')
+                        ->get()
+                        ->first();
+                    
+                    if($pr) {  
+                        $prs[] = $pr->toArray();
+                    }
                 }
+
+                $collect = collect($prs);
+                $max = $collect->max('total') ?? 0;
+                
+                $tt = $collect->map(function($v) use ($interval) {
+                    $v['owm_result'] = ($v['total'] * $interval) * $v['owm'];
+                    $v['vm_result'] = ($v['total'] * $interval) * $v['vm'];
+                    $v['odm_result'] = ($v['total'] * $interval) * $v['odm'];
+                    $v['nos_result'] = ($v['total'] * $interval) * $v['nos'];
+
+                    $v['total_2'] = $v['total'] * $interval;
+                    return $v;
+                })->all();
+                
+                $count = collect($tt)->sum('total_2');
+
+                $owm = $count > 0 ? (collect($tt)->sum('owm_result') / $count): 0;
+                $vm = $count > 0 ? (collect($tt)->sum('vm_result') / $count): 0;
+                $odm = $count > 0 ? (collect($tt)->sum('odm_result') / $count): 0;
+                $nos = $count > 0 ? (collect($tt)->sum('nos_result') / $count): 0;
+            
+                FossnirReportDaily::updateOrCreate([
+                    'mill_id' => $dir->id,
+                    'group_id' => $group->id,
+                    'sample_date' => $now->format('Y-m-d')
+                ], [
+                    'owm' => $owm,
+                    'vm' => $vm,
+                    'odm' => $odm,
+                    'nos' => $nos,
+                ]);
             }
-            $collect = collect($prs);
-            $max = $collect->max('total') ?? 0;
-            
-            $tt = $collect->map(function($v) use ($interval) {
-                $v['owm_result'] = ($v['total'] * $interval) * $v['owm'];
-                $v['vm_result'] = ($v['total'] * $interval) * $v['vm'];
-                $v['odm_result'] = ($v['total'] * $interval) * $v['odm'];
-                $v['nos_result'] = ($v['total'] * $interval) * $v['nos'];
-
-                $v['total_2'] = $v['total'] * $interval;
-                return $v;
-            })->all();
-            
-            $count = collect($tt)->sum('total_2');
-
-            $owm = $count > 0 ? (collect($tt)->sum('owm_result') / $count): null;
-            $vm = $count > 0 ? (collect($tt)->sum('vm_result') / $count): null;
-            $odm = $count > 0 ? (collect($tt)->sum('odm_result') / $count): null;
-            $nos = $count > 0 ? (collect($tt)->sum('nos_result') / $count): null;
-
-            
         }
     }
 }
