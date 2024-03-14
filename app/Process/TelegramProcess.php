d6e683ae3622388502e047fdbf917071cc23455c
Abdul Hafidz Anshari <aditans88@gmail.com>
2024-03-14 11:57:15 +0700

    update

---
+++ app/Process/TelegramProcess.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Process;
+
+use App\Service\Telegram;
+use Hyperf\Process\AbstractProcess;
+use Hyperf\Process\Annotation\Process;
+
+#[Process(name: "telegram_process")]
+class TelegramProcess extends AbstractProcess
+{
+    public function handle(): void
+    {
+        $t = make(Telegram::class);
+        $t->listen();
+    }
+
+    public function isEnable($server): bool
+    {
+        return true;
+    }
+}
