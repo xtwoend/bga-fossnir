c035c3bf618d39e1fef22011356d733c6166fbcf
Abdul Hafidz Anshari <aditans88@gmail.com>
2024-03-22 22:34:47 +0700

    update

--- app/Command/ReCountFossnirScore.php
+++ app/Command/ReCountFossnirScore.php
@@ -1,44 +1,28 @@
 <?php
 
 declare(strict_types=1);
-/**
- * This file is part of Hyperf.
- *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
- */
 
 namespace App\Command;
 
 use Carbon\Carbon;
-use App\Model\Group;
-use App\Model\FossnirDir;
-use App\Model\FossnirData;
-use App\Model\FossnirScore;
-use App\Model\GroupProduct;
-use Hyperf\DbConnection\Db;
 use App\Handler\FossnirHandler;
-use App\Model\FossnirThreshold;
-use App\Model\FossnirReportDaily;
 use Psr\Container\ContainerInterface;
 use Hyperf\Command\Annotation\Command;
 use Hyperf\Command\Command as HyperfCommand;
 use Symfony\Component\Console\Input\InputArgument;
 
 #[Command]
-class FossnirDailyReportCommand extends HyperfCommand
+class ReCountFossnirScore extends HyperfCommand
 {
     public function __construct(protected ContainerInterface $container)
     {
-        parent::__construct('fossnir:daily-count');
+        parent::__construct('fossnir:recount-score');
     }
 
     public function configure()
     {
         parent::configure();
-        $this->setDescription('Save daily result fossnir all mill');
+        $this->setDescription('ReCount score fossnir');
     }
 
     public function handle()
@@ -46,8 +30,17 @@ class FossnirDailyReportCommand extends HyperfCommand
         $date = $this->input->getArgument('date');
         $date = $date ? Carbon::parse($date) : Carbon::now();
 
-        $handler = new FossnirHandler;
-        $handler->count($date);
+        $diff = Carbon::now()->diffInDays($date);
+        
+        if($diff > 0) {
+            $handler = new FossnirHandler;
+            for($i = 0; $i < $diff; $i++) {
+                $nd = $date->copy()->addDay($i);
+                var_dump($nd->format('c'));
+                $handler->count($nd);
+            }
+        }
+
     }
 
     protected function getArguments()
---
+++ app/Handler/FossnirHandler.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Handler;
+
+use Carbon\Carbon;
+use App\Model\FossnirDir;
+use App\Model\FossnirData;
+use App\Model\FossnirScore;
+use App\Model\GroupProduct;
+use Hyperf\DbConnection\Db;
+use App\Model\FossnirThreshold;
+
+class FossnirHandler
+{
+    public function count($date)
+    {
+        $hour = (int) $date->format('H');
+        if($hour < 5) {
+            $date = $date->subDay();
+        }
+
+        foreach(FossnirDir::orderBy('order')->get() as $mill)
+        {
+            $groups = GroupProduct::where('mill_id', $mill->id)->get();
+            foreach($groups as $group)
+            {
+                $thresholds = FossnirThreshold::where('mill_id', $mill->id)
+                    ->where('group_id', $group->group_id)    
+                    ->whereIn('parameter', ['owm', 'vm', 'odm', 'nos'])
+                    ->get();
+
+                $score = FossnirScore::table($mill->id)->updateOrCreate([
+                    'sample_date' => $date->format('Y-m-d'),
+                    'mill_id' => $mill->id,
+                    'product_name' => $group->product_name,
+                ],[
+                    'threshold_owm' => ($thresholds?->where('parameter', 'owm')->first()?->threshold) ?: 0,
+                    'threshold_vm' => ($thresholds?->where('parameter', 'vm')->first()?->threshold) ?: 0,
+                    'threshold_odm' => ($thresholds?->where('parameter', 'odm')->first()?->threshold) ?: 0,
+                    'threshold_nos' => ($thresholds?->where('parameter', 'nos')->first()?->threshold) ?: 0,
+                ]);
+
+                $sDate = $score->sample_date->format('Y-m-d');
+
+                // query to read data
+                $count = FossnirData::table($mill->id)
+                    ->select(
+                        Db::raw("
+                            COUNT(*) as sample_count, 
+                            COUNT(IF(owm <= {$score->threshold_owm}, 1, NULL)) AS conconformance_owm, 
+                            COUNT(IF(vm <= {$score->threshold_vm}, 1, NULL)) AS conconformance_vm, 
+                            COUNT(IF(odm <= {$score->threshold_odm}, 1, NULL)) AS conconformance_odm, 
+                            COUNT(IF(nos <= {$score->threshold_nos}, 1, NULL)) AS conconformance_nos,
+                            SUM(owm) AS owm,
+                            SUM(vm) AS vm,
+                            SUM(odm) AS odm,
+                            SUM(nos) AS nos"
+                    ))
+                    ->where('product_name', $group->product_name)
+                    ->where('sample_date', '>=', Carbon::parse($sDate . ' 05:00:00')->format('Y-m-d H:i:s'))
+                    ->where('sample_date', '<', Carbon::parse($sDate . ' 05:00:00')->addDay()->format('Y-m-d H:i:s'))
+                    ->get()
+                    ->first();
+
+                $score->sample_count = $count->sample_count;
+                $score->score_owm = $count->conconformance_owm;
+                $score->score_vm = $count->conconformance_vm;
+                $score->score_odm = $count->conconformance_odm;
+                $score->score_nos = $count->conconformance_nos;
+
+                $score->owm = $count->owm ?: 0;
+                $score->vm = $count->vm ?: 0;
+                $score->odm = $count->odm ?: 0;
+                $score->nos = $count->nos ?: 0;
+ 
+                $score->save();
+            }
+        }
+    }
+}
